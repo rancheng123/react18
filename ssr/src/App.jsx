2c2e13eb8bb65b2eb8f943742a6f537f99e9272b
wuchuan <wuchuan_xiaoge@163.com>
2024-05-12 22:54:22 +0800

    RN-000: test ssr

--- ssr/src/App.jsx
+++ ssr/src/App.jsx
@@ -3,6 +3,7 @@ import Util from '@/components/page/util/util.jsx'
 
 const getData = async () => {
 <<<<<<< HEAD
+<<<<<<< HEAD
   return await import('../testData.js').then(res => res.default)
 };
 
@@ -18,16 +19,26 @@ export default function App() {
   }, [])
 =======
   await import('../testData').then(res => res.default)
+=======
+  return await import('../testData.js').then(res => res.default)
+>>>>>>> f84b245 (RN-000: test ssr)
 };
 
 export default function App() {
-  const [Page, setPage] = useState(null);
+  const [Page, setPage] = useState(false);
   useEffect(async () => {
     const data = await getData();
-    const res = await Util.loadComponent(pub.type == 'pc' ? 'html' : 'mo', data);
+    console.log(data)
+    const res = await Util.loadComponent('html', data);
+    // const res = await Util.loadComponent(pub.type == 'pc' ? 'html' : 'mo', data);
+    console.log(res, '====')
     setPage(res);
+<<<<<<< HEAD
   })
 >>>>>>> 470503b (RN-000: 测试服务)
+=======
+  }, [])
+>>>>>>> f84b245 (RN-000: test ssr)
   if (!Page) {
     return (
       <div>
@@ -36,9 +47,13 @@ export default function App() {
     )
   }
 <<<<<<< HEAD
+<<<<<<< HEAD
   return 'hello'
 =======
 
 >>>>>>> 470503b (RN-000: 测试服务)
+=======
+  return 'hello'
+>>>>>>> f84b245 (RN-000: test ssr)
   return <Page />
 }
