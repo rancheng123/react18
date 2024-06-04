be58bf32816e95d917cb23f6c1878cd2f96e2789
wuchuan <wuchuan_xiaoge@163.com>
2024-06-05 00:56:01 +0800

    RN-000: 修改bug

--- src/components/page/util/util.jsx
+++ src/components/page/util/util.jsx
@@ -24,7 +24,7 @@ export { lan };
 */
 function getQueryParam(url) {
   // 通过 ? 分割获取后面的参数字符串
-  let urlStr = url.split('?')[1]
+  let urlStr = url.split('?')[1] || ''
   // 创建空对象存储参数
   let obj = {};
   // 再通过 & 将每一个参数单独分割出来
