88e0f17a0d000143e28e7f3b06ca914383c4636b
wuchuan <wuchuan@fangcaojiaoyu.cn>
2024-06-12 17:37:42 +0800

    RN-000 change 冲突

--- ssr/src/entry-client.jsx
+++ ssr/src/entry-client.jsx
@@ -1,15 +1,7 @@
 import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
-<<<<<<< HEAD
 import App from './App'
-=======
-import App from '../../src/App'
->>>>>>> 470503b (RN-000: 测试服务)
-=======
-import App from './App'
->>>>>>> f84b245 (RN-000: test ssr)
 
 ReactDOM.hydrateRoot(
   document.getElementById('root'),
--- ssr/src/entry-server.jsx
+++ ssr/src/entry-server.jsx
@@ -1,14 +1,6 @@
 import React from 'react'
 import { renderToPipeableStream } from 'react-dom/server'
-<<<<<<< HEAD
-<<<<<<< HEAD
 import App from './App'
-=======
-import App from '../../src/App'
->>>>>>> 470503b (RN-000: 测试服务)
-=======
-import App from './App'
->>>>>>> f84b245 (RN-000: test ssr)
 
 /**
  * @param {string} url
