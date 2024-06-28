c9617bf3fb1ae88662093a47fe7d738422fbbc82
zcx <2099946717@qq.com>
2024-06-28 11:01:29 +0800

    languages 1

--- src/components/languages/view/html/flag_nation_name_s1_view.jsx
+++ src/components/languages/view/html/flag_nation_name_s1_view.jsx
@@ -1,5 +1,7 @@
 
 import React from 'react';
+import Util from "@/components/page/util/util.jsx";
+import Component from "@/components/text/view/components/component.jsx";
 
 function s1() {
     const {
@@ -18,11 +20,45 @@ function s1() {
                 clone
             }
         } = this
+
+
+    let {
+        link,
+        text = "",
+        hidden,
+        dataSource,
+        overflowPart = "automatic",
+        fontLabel,
+        language
+    } = document_data;
+
+    let _daSource = dataSource && dataSource.companyField; //数据源存在时，给控件data-source的自定义属性，用于查找数据源
+    let detailIClassenlarge = Component.getDetailClass(_daSource);
+
+    let hiddenClass = ""; //hidden存在,证明控件要隐藏
+
+    if (hidden) {
+        //预览时不返回结构,编辑页用class控件隐藏
+        if (Util.source) {
+            return null;
+        } else {
+            hiddenClass = "hidden";
+        }
+    }
+
     return (
-        <div id={id}>
-            1111122222
-        </div>
+        <Util.linkDecorator
+            link={Util.setLinkUrl(context.link, link)}
+            type="html"
+            id={id}
+            className={`listTxt ${id}A ${overflowPart} ${hiddenClass} ${detailIClassenlarge}`}
+        >
+            <div>
+                1111122222
+            </div>
+        </Util.linkDecorator>
+
     )
 }
 
-export { s1 }
+export {s1}
